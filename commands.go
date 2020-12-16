6ac8cfdd7f8669ebce84ada0c8d56104c8a258b8
Sapp3465 <cryptosap35@gmail.com>
2020-12-16 23:58:07 +0200

    Added commands

--- commands.go
+++ commands.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	“fmt”
+)
+
+type printCommand struct {
+	output string
+}
+
+func (print *printCommand) Execute(loop Handler) {
+	fmt.Println(print.output)
+}
+  
+type catCommand struct {
+	arg1, arg2 string
+}
+  
+func (cat *catCommand) Execute(loop Handler) {
+	res := cat.arg1 + cat.arg2
+	loop.Post(&printCommand {output: res})
+}
