6ed9b9571ecd91e1b9dffe1e4a55f599a67e3f3d
andreichenko256 <andreichenko.kirill2000@gmail.com>
2020-12-16 23:50:36 +0200

    engine.go added

--- engine/engine.go
+++ engine/engine.go
@@ -1,1 +1,88 @@
 package engine
+
+import (
+	"sync"
+)
+
+type Command interface {
+	Execute(handler Handler)
+}
+
+type Handler interface {
+	Post(cmd Command)
+}
+
+type messageQueue struct {
+	sync.Mutex
+	data []Command
+	receiveSignal chan struct{}
+	receiveRequested bool
+}
+
+type Loop struct {
+	mq *messageQueue
+	stopSignal chan struct{}
+	stopRequest bool
+}
+
+func (mq *messageQueue) push(command Command){
+	mq.Lock()
+	defer mq.Unlock()
+
+	mq.data = append(mq.data, command)
+	if mq.receiveRequested {
+		mq.receiveRequested = false
+		mq.receiveSignal <- struct{}{}
+	}
+
+}
+
+func (mq *messageQueue) pull() Command{
+	mq.Lock()
+	defer mq.Unlock()
+
+	if mq.empty() {
+		mq.receiveRequested = true
+		mq.Unlock()
+		<- mq.receiveSignal
+		mq.Lock()
+	}
+
+	res := mq.data[0]
+	mq.data[0] = nil
+	mq.data = mq.data[1:]
+	return res
+}
+
+func (mq *messageQueue) empty() bool {
+	return len(mq.data) == 0
+}
+
+func (l *Loop) Start() {
+	l.mq = &messageQueue{receiveSignal: make(chan struct{})}
+	l.stopSignal = make (chan struct{})
+	go func ()  {
+		for !l.stopRequest || !l.mq.empty(){
+			cmd := l.mq.pull()
+			cmd.Execute(l)
+		}
+		l.stopSignal <- struct{}{}
+	}()
+}
+
+func (l *Loop) Post(cmd Command) {
+	l.mq.push(cmd)
+}
+
+type CommandFunc func (h Handler)
+
+func (cf CommandFunc) Execute (h Handler){
+	cf(h)
+}
+
+func (l *Loop) AwaitFinish() {
+	l.Post(CommandFunc(func(h Handler){
+		l.stopRequest = true
+	}))
+	<- l.stopSignal
+} 
