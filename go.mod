079ee25cbaefdac51aa2b4955028fddd1ca59089
andreichenko256 <andreichenko.kirill2000@gmail.com>
2020-12-17 00:52:48 +0200

    Fix

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KPI-KMD/Lab4
+
+go 1.15
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func parser(commandLine string) engine.Command{
 	parts := strings.Fields(commandLine)
 
 	if len(parts) == 0{
-		return &printCommand{output: “SYNTAX ERROR: no arguments”}
+		return &printCommand{output: "SYNTAX ERROR: no arguments"}
 	}
 
 	switch parts[0] {
@@ -19,36 +19,36 @@ func parser(commandLine string) engine.Command{
 		if len(parts) == 2 {
 			return &printCommand{output: parts[1]}
 		} else if len(parts) == 1{
-			return &printCommand{output: “SYNTAX ERROR: not enough arguments”}
+			return &printCommand{output: "SYNTAX ERROR: not enough arguments"}
 		} else {
-			return &printCommand{output: “SYNTAX ERROR: too many arguments”}
+			return &printCommand{output: "SYNTAX ERROR: too many arguments"}
 		}
 	case "cat":
 		if len(parts) < 3 {
-			return &printCommand{output: “SYNTAX ERROR: not enough arguments”}
+			return &printCommand{output: "SYNTAX ERROR: not enough arguments"}
 		} else if len(parts) > 3 {
-			return &printCommand{output: “SYNTAX ERROR: too many arguments”}
+			return &printCommand{output: "SYNTAX ERROR: too many arguments"}
 		} else {
 			return &catCommand{arg1: parts[1], arg2: parts[2]}
 		}
 	default:
-		return &printCommand{output: “SYNTAX ERROR: command not found”}
+		return &printCommand{output: "SYNTAX ERROR: command not found"}
 	}
 }
 
 
 func main() {
-	eventLoop := new(engine.EventLoop)
-	eventLoop.Start()
+	Loop := new(engine.Loop)
+	Loop.Start()
 
-	if input, err := os.Open(inputFile.txt); err == nil {
+	if input, err := os.Open("inputFile.txt"); err == nil {
 		defer input.Close()
 		scanner := bufio.NewScanner()
 		for scanner.Scan() {
 			commandLine := scanner.Text()
-			cmd := parse(commandLine) 
-			eventLoop.Post(cmd)
+			cmd := parser(commandLine) 
+			Loop.Post(cmd)
 		}
 	} 
-	eventLoop.AwaitFinish()
+	Loop.AwaitFinish()
 }
